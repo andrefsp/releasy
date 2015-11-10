8e6ef8cbdf36fee05be0914450796b2997fe0438
andrefsp <andrefsp@gmail.com>
2015-11-10 11:55:03 +0000

    Add setup for local development

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Releasy",
+    version="0.1",
+    packages=find_packages(),
+    author="Andre da Palma",
+    author_email="andrefsp@gmail.com",
+    url="https://github.com/andrefsp/releasy/",
+    description="Pyppo is a small flow based programming micro framework",
+    keywords = ['git', 'management', 'release'],
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    requires=['gitpython(==1.0.1)']
+)
